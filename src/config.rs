c0732e34de37d18b6e8f82f97f1229211644ecbd
Marek Licko <mareklicko448@gmail.com>
2023-11-28 19:37:01 +0100

    Some config changes

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,7 @@ use macroquad::prelude::*;
 
 pub static WINDOW_SIZE_PX: Vec2 = Vec2::from_array([1920., 1200.]);
 
-pub const GRID_SIZE: Vec2 = Vec2::from_array([1920., 1200.]);
+pub const GRID_SIZE: Vec2 = Vec2::from_array([1920. / 2., 1200. / 2.]);
 pub const NUM_CONNECTIONS: usize = 3;
 pub static SQUARE_MODE: bool = true;
 pub static MAX_DISTANCE_TO_CONNECT: i32 = 3; // Has no effect if square mode is flase
