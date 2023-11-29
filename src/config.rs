edfcee7de869619a950d180f6725341229c4f723
Marek Licko <mareklicko448@gmail.com>
2023-11-29 17:17:21 +0100

    OOOOOOOOOO

--- src/config.rs
+++ src/config.rs
@@ -2,10 +2,10 @@ use macroquad::prelude::*;
 
 pub static WINDOW_SIZE_PX: Vec2 = Vec2::from_array([1920., 1200.]);
 
-pub const GRID_SIZE: Vec2 = Vec2::from_array([1920. / 2., 1200. / 2.]);
-pub const NUM_CONNECTIONS: usize = 3;
+pub const GRID_SIZE: Vec2 = Vec2::from_array([1920. / 4., 1200. / 4.]);
 pub static SQUARE_MODE: bool = true;
-pub static MAX_DISTANCE_TO_CONNECT: i32 = 3; // Has no effect if square mode is flase
+pub const NUM_CONNECTIONS: usize = 3;
+pub static MAX_DISTANCE_TO_CONNECT: i32 = 3; // Has no effect if square mode is off
 pub static SHUFFLE_DIFFS: bool = true;
 
 pub static MAX_CORES: usize = 16;
